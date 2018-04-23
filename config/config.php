72a1df60094a773b4f13e6ccf9e30f106b994a3c
Sulymern59 <sulymern59@gmail.com>
2018-04-23 02:22:17 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "myexam");
+define("DB_HOST", "rguexams-mysqldbserver.mysql.database.azure.com");
+define("DB_USER", "mysqldbuser@rguexams-mysqldbserver");
+define("DB_PASS", "Suleiman59");
+define("DB_NAME", "mysqldatabase4142");
 define("TITLE", "");
 ?>
